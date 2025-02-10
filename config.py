8c96329a5ae7fd4b880348c7732dac000ed1a542
orgservice <146081596+orgservice@users.noreply.github.com>
2025-02-10 23:23:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,17 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", "25465082"))
+    APP_ID = int(os.environ.get("APP_ID", '25465082'))
     API_HASH = os.environ.get("API_HASH", "4a6b5e40c8bc08c8af09add6cca23b18")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7821525708:AAFkeXtKLVd3v9K-ZUPx3K4cP6n5uuc7Ajk")
     LOGS_CHANNEL = (
-        int(os.environ.get("LOGS_CHANNEL", "-1001628560294"))
+        int(os.environ.get("LOGS_CHANNEL", '-1001628560294'))
         #if os.environ.get("LOGS_CHANNEL").strip("-").isdigit()
         #else os.environ.get("LOGS_CHANNEL")
     )
     MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://uhdprime:uhdprime@cluster0.ry5y4yk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     MONGODB_DBNAME = os.environ.get("MONGODB_DBNAME", "Unzipper_Bot")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "6487118438"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", '6487118438'))
     DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
     THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
