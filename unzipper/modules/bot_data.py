093f681aaceac5ca245fa34db6be52936d9641be
orgservice <146081596+orgservice@users.noreply.github.com>
2025-02-11 13:11:05 +0530

    Update bot_data.py

--- unzipper/modules/bot_data.py
+++ unzipper/modules/bot_data.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2022 - 2024 EDM115
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
@@ -67,15 +68,16 @@ Don't go AFK next time 😉
     # database.py
 
     BANNED = """
-**Sorry, You're banned 💀**
+**Sorry you're banned 💀**
 
-Report this at @ORGSupports if you think this is a mistake, I may unban you.
+Report this at @EDM115_chat if you think this is a mistake, I may unban you
     """
 
     NEW_USER_BAD = """
 **#NEW_USER** 🎙
 
 **User profile :** `{}`
+`[AttributeError]`
     """
 
     NEW_USER = """
@@ -97,6 +99,8 @@ This may take a while, go grab a coffee ☕️
     PROGRESS_MSG = """
 {}
 {}
+
+**Powered by @EDM115bots**
     """
 
     PROCESSING = "**Processing…**"
@@ -117,7 +121,7 @@ This may take a while, go grab a coffee ☕️
 
     LOG_CHECKED = "Log channel checked"
 
-    BOT_RUNNING = "Bot is running now !"
+    BOT_RUNNING = "Bot is running now ! Join @EDM115bots"
 
     WRONG_LOG = """
 Error : the provided **LOGS_CHANNEL** (`{}`) is incorrect
@@ -135,21 +139,25 @@ Sorry, the bot is currently full 🥺
     CHOOSE_EXT_MODE = """
 Select the extraction mode for that {} 👀
 
-{} : **Extract Zip**
+{} : **Normal mode**
+🔐 : **Password protected**
+🖼️ : **Change the thumbnail**
+✏ : **Change the thumbnail and rename the file**
 ❌ : **Cancel your task**
     """
 
     CHOOSE_EXT_MODE_MERGE = """
 Select the extraction mode for that merged file 👀
 
-🗂️ : **Extract Zip**
+🗂️ : **Normal mode**
+🔐 : **Password protected**
 ❌ : **Cancel your task**
     """
 
     EXT_CAPTION = """
 `{}`
 
-Successfully Extracted. 🥰
+Successfully extracted by @unzip_edm115bot 🥰
     """
 
     REPORT_TEXT = """
@@ -182,7 +190,7 @@ have been saved from the URL
 
 **⚠ IN ALL CASES ⚠**, please send **/clean**, else you can't send any other task 🙂🔫
 
-Please Report this at @ORGSupports if you think this is a serious error.
+Please report this at @EDM115_chat if you think this is a serious error
     """
 
     HOW_MANY_UPLOADED = "`{}` file(s) have been extracted from that archive"
@@ -217,7 +225,7 @@ Use **/merge** to start one
 
     """
 
-    REFRESH_STATS = "Refreshing Stats… ♻️"
+    REFRESH_STATS = "Refreshing stats… ♻️"
 
     ACTUAL_THUMB = "Your actual thumbnail"
 
@@ -227,43 +235,148 @@ Hi **{}** 👋, I'm **Unarchiver bot** 🥰
 I can extract any archive, with password or not, splitted, …
 Send **/commands** to learn more
 
-**Made with ❤️ by @AdultService**
+**Made with ❤️ by @EDM115bots**
+**/donate** if you can 🥺
     """
 
     HELP_TXT = """
 **• How to extract 🤔**
 
-**1)** Send the file or link that you want to extract
-**2)** Click on extract button (If you sent a link use `🔗` button. If it's a file just use `🗂️` button)
+    **1)** Send the file or link that you want to extract
+    **2)** Click on extract button (If you sent a link use `🔗` button. If it's a file just use `🗂️` button)
 
 
 **• How to change upload mode 🤔**
     Send **/mode**
 
+
 **Note :**
     **1.** If your archive is password protected select `🔐` button
     **2.** Please don't send corrupted files ! If you sent one by mistake just send **/clean**
     **3.** If your archive have +95 files in it then bot can't show all of extracted files to select from (yet). So in that case if you can't see your file in the buttons just click on `Upload all 📤` button. It will send all extracted files to you !
+
+
+**• Got an error ?**
+    Visit edm115.dev/unzip#help
+
+
+**• I wanna have help 🥺**
+
+    PM me at **@EDM115** or join the chat **@EDM115_chat**
     """
 
     ABOUT_TXT = """
 **About Unarchiver bot [v6.3.5]**
 
-• **Language :** [Python 3.10.8](https://www.python.org/)
-• **Framework :** [Pyrogram](https://pyrogram.org/)
-• **Developer :** [@ORGSupports](https://t.me/ORGSupports)
+• **Language :** [Python 3.12.4](https://www.python.org/)
+• **Framework :** [Pyrogram 2.0.106](https://pyrogram.org/)
+• **Source code :** [EDM115/unzip-bot](https://github.com/EDM115/unzip-bot)
+• **Developer :** [EDM115](https://github.com/EDM115)
 
-Made with ❤️ by **@AdultService**
+**[Rate me ⭐](https://t.me/BotsArchive/2705)**
+Made with ❤️ by **@EDM115bots**
     """
 
     DONATE_TEXT = """
-No Need To Donate 😊
+I'm going to be honest : **this bot costs me money**…
+Nothing's free on this world, however I try to keep this bot for free for as many people as possible
+I don't like to put restrictions, nor getting your PM's flooded with ads…
+
+So if you can, donate :)
+It helps out a ton, covers the costs (hosting, updating, … 👨‍💻)
+
+--How ?--
+• **[Paypal](https://www.paypal.me/8EDM115)**
+• **[GitHub Sponsors](https://github.com/sponsors/EDM115)**
+• **[Directly in Telegram](https://t.me/EDM115bots/698)**
+• **[BuyMeACoffee](https://www.buymeacoffee.com/edm115)**
+
+Thanks for your contribution 😊
+
+--Side note :--
+Donation doesn't count as a VIP subscription. Check **/vip** for more info
+    """
+
+    VIP_INFO = """
+--**NOT AVAILABLE YET !**--
+
+Wanna help the developer of this __amazing__ bot ?
+Here's how : Become a VIP user and benefit from extra perks !
+
+**VIP perks :**
+- No max tasks limit
+- No AFK timeout
+- Get a better support
+- Upload files up to 4Gb
+- Early access to new features
+- Access a second bot exclusive to VIPs __(subject to conditions)__
+- And more…
+
+**What's the price ?**
+- `1$/month`
+- `10$/year`
+
+**How to become a VIP ?**
+1) Send **/pay** to the bot
+2) Choose your subscription
+3) Send a screenshot of your payment to **@EDM115**
+4) Enjoy your VIP perks !
+
+**What happens when my subscription ends ?**
+If you choosed GitHub Sponsors, Telegram Donate or BuyMeACoffee, you will be automatically renewed until you cancel it
+If you choosed PayPal, you will have to redo the 4 above steps
+You will be notified few days before you subscription ends so you can check if you want to renew it or not
+
+**I wanna cancel my subscription**
+Just send **/stoppay** and follow the instructions according to the platform you selected
+Your payment will be cancelled and you will keep your VIP perks until the end of your subscription
+(i.e. if you paid for 1 month, from 05/01/2024 to 05/02/2024 and you cancel your subscription on 15/01/2024, your perks will stay until 05/02/2024)
+
+**What is the referral system ?**
+Referrals have benefits for both sides :)
+- For the referrer : you get 1 month of VIP for free for each 3 new VIPs you bring
+- For the referred : you get 1 month of VIP for free if you take the monthly subscription, and 3 months for free if you take the yearly subscription
+How to input the referral code ? Just send **/pay** to the bot and follow the instructions
+    """
+
+    VIP_REQUIRED_MESSAGE = """
+Use this command as a reply to a messsage, where you have the following (ONE PER LINE) :
+The user ID (int)
+When the subscription starts (in %Y-%m-%dT%H:%M:%SZ format)
+When the subscription ends (same)
+Which platform had been used [paypal, telegram, sponsor, bmac]
+At which frequency the user is billed [monthly, yearly]
+Is the user a early supporter (can be obtained only the first 3 months) [True, False]
+Is the user also a donator [True, False]
+When does the user ever started a subscription (date)
+How many successful payments had been done (int)
+Is there been any gap between payments [True, False]
+If the user had been gifted a Premium plan [True, False]
+The user referral code (str)
+Is this a lifetime free subscription [True, False]
+    """
+
+    VIP_ADDED_USER = """
+The following user had been added with the following infos :
+User ID : `{}`
+Start date : `{}`
+End date : `{}`
+Platform : `{}`
+Frequency : `{}`
+Early supporter : `{}`
+Donator : `{}`
+First subscription date : `{}`
+Successful payments : `{}`
+Gap between payments : `{}`
+Gifted : `{}`
+Referral code : `{}`
+Lifetime : `{}`
     """
 
     CLEAN_TXT = """
-**Are You Sure? Want to clean your task 🤔**
+**Are sure want to clean your task 🤔**
 
-Note : This action cannot be undone.
+Note : This action cannot be undone
     """
 
     SELECT_UPLOAD_MODE_TXT = """
@@ -426,7 +539,7 @@ Use the **/merge** command
     QUERY_PARSE_ERR = """
 Fatal query parsing error 💀
 
-Please contact @ORGSupports with details and Screenshots.
+Please contact @EDM115_chat with details and screenshots
     """
 
     GIVE_NEW_NAME = """
@@ -443,6 +556,8 @@ Please send the new file name (**--INCLUDE THE FILE EXTENTION !--**)
 
     UPLOADED = """
 **Successfully uploaded ✅**
+
+**Join @EDM115bots ❤️**
     """
 
     NO_FILE_LEFT = "There's no file left to upload"
@@ -456,6 +571,10 @@ Please send the new file name (**--INCLUDE THE FILE EXTENTION !--**)
     CANCELLED = "**Cancelled successfully ✅**"
 
     PROCESS_CANCELLED = "❌ Process cancelled"
+
+    # commands.py
+
+    PRIVACY = "PRIVACY ??"
 
     PROCESS_RUNNING = """
 Already one process is running, don't spam 😐
@@ -557,7 +676,7 @@ If it fails again, it means that {} haven't started the bot yet (or deleted the 
     REPORT_DONE = """
 Report sucessfully sent ! An answer will arrive soon
 
-Note : if you need to reply to replies, always use that /report command.
+Note : if you need to reply to replies, always use that /report command (or join **@EDM115_chat**)
     """
 
     BAN_ID = "Give an user id to ban 😈"
@@ -592,6 +711,48 @@ Note : if you need to reply to replies, always use that /report command.
 **User ID :** `{}`
     """
 
+    INFO = "Send a text (shorter possible) from any user/chat. And you will have infos about it 👀"
+
+    USER = "This is a WIP command that would allow you to get more stats about your utilisation of me 🤓"
+
+    UNABLE_FETCH = "Unable to fetch"
+
+    USER_INFO = """
+**User ID :** `{}`
+`{}` files uploaded
+…
+
+WIP
+    """
+
+    UID_UNAME_INVALID = "Error happened, The user ID/username is probably invalid"
+
+    USER2_INFO = """
+`{}`
+
+**Direct link to profile :** tg://user?id={}
+    """
+
+    MAINTENANCE = """
+Do you want the bot to go in maintenance mode 🤔
+Current state : `{}`
+    """
+
+    MAINTENANCE_ASK = """
+False : No maintenance
+True : Maintenance
+Send the appropriate string
+    """
+
+    MAINTENANCE_DONE = "Successfully changed maintenance mode to `{}`"
+
+    MAINTENANCE_ON = """
+Maintenance mode is currently **ON**
+Tasks can't be processed. Come back later
+    """
+
+    MAINTENANCE_FAIL = "Provide one of the values"
+
     NO_THUMBS = "No thumbnails on the server yet"
 
     ERASE_ALL = "**Cleaning…**"
@@ -612,6 +773,12 @@ Note : if you need to reply to replies, always use that /report command.
 
     RESTARTING = "{} : Restarting…"
 
+    PULLING = "Pulling updates… ⌛"
+
+    PULLED = "✅ Pulled changes, restarting…"
+
+    NO_PULL = "Nothing to pull 😅"
+
     COMMANDS_LIST = """
 Here is the list of the commands you can use (only in private btw) :
 
@@ -619,30 +786,43 @@ Here is the list of the commands you can use (only in private btw) :
 **/start** : To know if I'm online
 **/help** : Gives a simple help
 **/about** : Know more about me
+**/donate** : Know how you can contribute to this bot
 **/clean** : Remove your files from my server. Also useful if a task failed
 **/mode** : Change your upload mode (either `doc` or `media`)
 **/stats** : Know all the current stats about me
 **/merge** : Merge splitted archives together
 **/done** : After you sent all the splitted archives, use this to merge them
+**/info** : Get full info about a [Message](https://docs.pyrogram.org/api/types/Message) (info returned by Pyrogram)
 **/addthumb** : Upload with a custom thumbnail
 **/delthumb** : Removes your thumbnail
 **/report** : Used by replying to a message, sends it to the bot owner (useful for bug report, or any question)
+**/vip** : __Not available yet__ Know more about the VIP subscription
+**/commands** : This message
     """
 
     ADMINCMD = """
 Here's all the commands that only the owner (you) can use :
 
-**/commands** : Users Commands
+**/gitpull** : Pulls the latest changes from GitHub
 **/broadcast** : Send something to all the users
+**/sendto {user_id}** : Same as broadcast but for a single user. Doesn't handle replies for now…
 **/ban {user_id}** : Ban an user. He can no longer use your bot, except if…
 **/unban {user_id}** : …you unban him. All his stats and settings stays saved after a ban
+**/user {user_id}** : Know more about the use of your bot by a single user
+**/user2 {user_id}** : Get full info about an [User](https://docs.pyrogram.org/api/types/User) (info returned by Pyrogram)
+**/self** : Get full info about me (info returned by Pyrogram)
 **/getthumbs** : Get all the thumbnails on the server
+**/redbutton** : __Does nothing yet__
+**/maintenance** : Put the bot in or out of maintenance mode. No tasks can be processed while on
 **/cleanall** : Same as `/clean`, but for the whole server
 **/cleantasks** : Same as `/cleanall`, plus removes them from the database
 **/logs** : Send you the logs (all of them). Useful for bug tracking. Send them to **@EDM115** if you don't understand them/need help
 **/restart** : Does a basic restart, less intrusive as the `/redbutton` one
+**/dbexport** : ~~Exports the whole database as CSV~~ __Does nothing yet__
 **/eval {code}** : Evaluate a piece of code. Useful for debugging
 **/exec {code}** : Execute a piece of code. Useful for debugging
+**/admincmd** : This message
+**/commands** : For all the other commands
     """
 
     # custom_thumbnail.py
